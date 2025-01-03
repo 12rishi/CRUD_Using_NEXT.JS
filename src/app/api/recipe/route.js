f27182851d613bc2a058df826543c35a8bdd6966
rishithapa <thapajack123@gmail.com>
2025-01-03 11:17:28 +0545

    implemented POST functionality and storing data in a particular db

--- src/app/api/recipe/route.js
+++ src/app/api/recipe/route.js
@@ -1,5 +1,32 @@
-export function GET() {
-  return Response.json({
-    message: "hello guyz",
-  });
-}
+import { db } from "@/db/db";
+import { recipes } from "@/db/schema";
+
+export const POST = async (request) => {
+  const data = await request.json();
+  try {
+    if (data) {
+      console.log(data.name);
+      const recipeData = await db.insert(recipes).values(data);
+      return Response.json(
+        {
+          message: recipeData,
+        },
+        { status: 201 }
+      );
+    } else {
+      return Response.json(
+        {
+          message: "please provide the data",
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return Response.json(
+      {
+        message: error?.message,
+      },
+      { status: 500 }
+    );
+  }
+};
